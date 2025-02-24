ff8108de0641756e5b1b19bf79443116a17e4da3
kamarini09 <moragiannikam@gmail.com>
2025-02-24 16:00:48 +0100

    end for today

--- src/entries/entries.module.ts
+++ src/entries/entries.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EntriesService } from './entries.service';
 import { EntriesController } from './entries.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Entry } from './entities/entry.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Entry])],
   controllers: [EntriesController],
   providers: [EntriesService],
 })
